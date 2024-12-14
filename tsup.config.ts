e2f4954c92df1876f75cbacd0678f4251ada2b6d
StefanStuehrmann <stefan.stuehrmann@outlook.com>
2024-12-14 22:31:40 +0000

    fix missing mjs

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["cjs", "esm"],
+  format: ["esm", "cjs"],
   dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
+  outExtension({ format }) {
+    return {
+      js: format === "esm" ? ".mjs" : ".js",
+    };
+  },
 });
